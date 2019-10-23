92f03a25e6932d0941f577cf897ce8da50d9b795
retroflexer <skolicha@redhat.com>
2019-10-22 20:57:15 -0400

    All calls to etcd (etcdserver) are implemented using clientv3.

---
+++ etcdutils_clientv3.go
@@ -0,0 +1,119 @@
+package etcdutils
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"time"
+
+	"github.com/coreos/etcd/pkg/fileutil"
+	"go.etcd.io/etcd/clientv3"
+	"go.etcd.io/etcd/clientv3/snapshot"
+	"go.etcd.io/etcd/embed"
+	"go.uber.org/zap"
+)
+
+func SaveSnapshot(ctx context.Context, cfg clientv3.Config, dbPath string) error {
+	if len(cfg.Endpoints) != 1 {
+		return fmt.Errorf("snapshot must be requested to one selected node, not multiple %#v", cfg.Endpoints)
+	}
+	cli, err := clientv3.New(cfg)
+	if err != nil {
+		return err
+	}
+	defer cli.Close()
+
+	partpath := dbPath + ".part"
+	defer os.RemoveAll(partpath)
+
+	var f *os.File
+	f, err = os.OpenFile(partpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, fileutil.PrivateFileMode)
+	if err != nil {
+		return fmt.Errorf("could not open %s (%v)", partpath, err)
+	}
+
+	now := time.Now()
+	var rd io.ReadCloser
+	rd, err = cli.Snapshot(ctx)
+	if err != nil {
+		return err
+	}
+
+	if _, err = io.Copy(f, rd); err != nil {
+		return err
+	}
+	if err = fileutil.Fsync(f); err != nil {
+		return err
+	}
+	if err = f.Close(); err != nil {
+		return err
+	}
+	log.Println(
+		"fetched snapshot",
+		cfg.Endpoints[0],
+		"took", time.Since(now),
+	)
+
+	if err = os.Rename(partpath, dbPath); err != nil {
+		return fmt.Errorf("could not rename %s to %s (%v)", partpath, dbPath, err)
+	}
+	log.Println("saved snapshot to path", dbPath)
+	return nil
+}
+
+func RestoreSnapshot(ctx context.Context, cfg embed.Config, peerURLs []string, dbPath string) error {
+	sp := snapshot.NewV3(zap.NewExample())
+	return sp.Restore(snapshot.RestoreConfig{
+		SnapshotPath:        dbPath,
+		Name:                cfg.Name,
+		OutputDataDir:       cfg.Dir,
+		PeerURLs:            peerURLs,
+		InitialCluster:      cfg.InitialCluster,
+		InitialClusterToken: cfg.InitialClusterToken,
+	})
+}
+
+func EtcdMemberAdd(ctx context.Context, cfg clientv3.Config, peerURLs []string) error {
+	cli, err := clientv3.New(cfg)
+	if err != nil {
+		return err
+	}
+	defer cli.Close()
+
+	mresp, err := cli.MemberAdd(ctx, peerURLs)
+	if err != nil {
+		return err
+	}
+	log.Println("added member.PeerURLs:", mresp.Member.PeerURLs)
+	return nil
+}
+
+func EtcdMemberRemove(ctx context.Context, cfg clientv3.Config, memberName string) error {
+	cli, err := clientv3.New(cfg)
+	if err != nil {
+		return err
+	}
+	defer cli.Close()
+
+	resp, err := cli.MemberList(ctx)
+	if err != nil {
+		return err
+	}
+
+	var id uint64 = 0
+	for _, m := range resp.Members {
+		if m.Name == memberName {
+			id = m.ID
+			break
+		}
+	}
+
+	if id == 0 {
+		return fmt.Errorf("member not found to remove")
+	}
+
+	_, err = cli.MemberRemove(ctx, id)
+	return err
+}
---
+++ go.mod
@@ -0,0 +1,34 @@
+module github.com/retroflexer/etcdutils
+
+go 1.12
+
+require (
+	github.com/coreos/bbolt v1.3.3 // indirect
+	github.com/coreos/etcd v3.3.17+incompatible
+	github.com/coreos/go-semver v0.3.0 // indirect
+	github.com/coreos/go-systemd v0.0.0-20190719114852-fd7a80b32e1f // indirect
+	github.com/coreos/pkg v0.0.0-20180928190104-399ea9e2e55f // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/google/btree v1.0.0 // indirect
+	github.com/google/uuid v1.1.1 // indirect
+	github.com/gorilla/websocket v1.4.1 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.1.0 // indirect
+	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.11.3 // indirect
+	github.com/jonboulle/clockwork v0.1.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/prometheus/client_golang v1.2.1 // indirect
+	github.com/soheilhy/cmux v0.1.4 // indirect
+	github.com/tmc/grpc-websocket-proxy v0.0.0-20190109142713-0ad062ec5ee5 // indirect
+	github.com/xiang90/probing v0.0.0-20190116061207-43a291ad63a2 // indirect
+	go.etcd.io/etcd v3.3.17+incompatible
+	go.uber.org/multierr v1.2.0 // indirect
+	go.uber.org/zap v1.11.0
+	golang.org/x/time v0.0.0-20190921001708-c4c64cad1fd0 // indirect
+	google.golang.org/appengine v1.4.0 // indirect
+	google.golang.org/genproto v0.0.0-20191009194640-548a555dbc03 // indirect
+	google.golang.org/grpc v1.24.0 // indirect
+	sigs.k8s.io/yaml v1.1.0 // indirect
+)
